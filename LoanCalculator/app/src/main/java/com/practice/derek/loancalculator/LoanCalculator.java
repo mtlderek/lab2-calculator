1eb0fe9b1654b06ec9e562fabdfae834cf1a6da9
unknown <derekmclean.mtl@gmail.com>
2017-09-07 20:20:22 -0400

    Added XML - UI is in place, no functionality yet

---
+++ LoanCalculator/app/src/main/java/com/practice/derek/loancalculator/LoanCalculator.java
@@ -0,0 +1,112 @@
+package com.practice.derek.loancalculator;
+
+public class LoanCalculator
+{
+    // TODO: 9/7/2017 Create a Loan Amount Field and Label
+    // TODO: 9/7/2017 Create a Loan Term (years) Field and Label
+    // TODO: 9/7/2017 Create yearly interest rate Field and Label
+    // TODO: 9/7/2017 Calculate btn 
+    // TODO: 9/7/2017 Clear btn
+    // TODO: 9/7/2017 Results editText
+    // TODO: 9/7/2017 Monthly payment EditText
+    // TODO: 9/7/2017 Total Payment EditText
+    // TODO: 9/7/2017 Total interest EditText
+    // TODO: 9/7/2017 Loan Calculator EditText
+
+    private double loanAmount;
+    private int numberOfYears;
+    private double yearlyInterestRate;
+
+    // no default no arg constructor generated if a constructor with args is delcared pmc
+    public LoanCalculator ()
+    {
+    }
+
+
+    public LoanCalculator (double loanAmount, int numberOfYears,
+                           double yearlyInterestRate)
+    {
+        this.loanAmount = loanAmount;
+        this.numberOfYears = numberOfYears;
+        this.yearlyInterestRate = yearlyInterestRate;
+    }
+
+
+    public double getLoanAmount ()
+    {
+        return loanAmount;
+    }
+
+
+    public double getMonthlyPayment ()
+    {
+        double monthlyPayment;
+        double monthlyInterestRate;
+        int numberOfPayments;
+        if (numberOfYears != 0 && yearlyInterestRate != 0)
+        {
+            //calculate the monthly payment
+            monthlyInterestRate = yearlyInterestRate / 1200;
+            numberOfPayments = numberOfYears * 12;
+
+            monthlyPayment =
+                    (loanAmount * monthlyInterestRate) /
+                            (1 - (1 / Math.pow ((1 + monthlyInterestRate), numberOfPayments)));
+
+            monthlyPayment = Math.round (monthlyPayment * 100) / 100.0;
+        }
+        else
+            monthlyPayment = 0;
+        return monthlyPayment;
+    }
+
+
+    public int getNumberOfYears ()
+    {
+        return numberOfYears;
+    }
+
+
+    public double getTotalCostOfLoan ()
+    {
+        return getMonthlyPayment () * numberOfYears * 12;
+
+    }
+
+
+    public double getTotalInterest ()
+    {
+        return getTotalCostOfLoan () - loanAmount;
+    }
+
+
+    public double getYearlyInterestRate ()
+    {
+        return yearlyInterestRate;
+    }
+
+
+    public void setLoanAmount (double loanAmount)
+    {
+        this.loanAmount = loanAmount;
+    }
+
+
+    public void setNumberOfYears (int numberOfYears)
+    {
+        this.numberOfYears = numberOfYears;
+    }
+
+
+    public void setYearlyInterestRate (double yearlyInterestRate)
+    {
+        this.yearlyInterestRate = yearlyInterestRate;
+    }
+
+
+    public String toString ()
+    {
+        return getLoanAmount () + "," + getNumberOfYears () + "," +
+                getYearlyInterestRate ();
+    }
+}
